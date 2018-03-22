3e95df524c8d3d65c9ea263592fab2174d610387
Ahmed Elhendawy <ahm_elhen@yahoo.com>
2018-03-22 14:47:37 +0200

    Ranking interface callback to process score value

--- app/src/main/java/com/example/android/onlinequizapp/fragments/RankingFragment.java
+++ app/src/main/java/com/example/android/onlinequizapp/fragments/RankingFragment.java
@@ -9,13 +9,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.onlinequizapp.R;
+import com.example.android.onlinequizapp.model.QuestionScore;
+import com.example.android.onlinequizapp.utils.Common;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RankingFragment extends Fragment {
 	
+	int sum = 0;
 	View myFragment;
+	FirebaseDatabase database;
+	DatabaseReference question_Score;
 	
 	public RankingFragment() {
 		// Required empty public constructor
@@ -30,6 +40,8 @@ public class RankingFragment extends Fragment {
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		database = FirebaseDatabase.getInstance();
+		question_Score = database.getReference("Question_Score");
 	}
 	
 	@Override
@@ -37,7 +49,26 @@ public class RankingFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		myFragment = inflater.inflate(R.layout.fragment_ranking, container, false);
+		updateScore(Common.currentUser.getUserName());
 		return myFragment;
+	}
+	
+	private void updateScore(String userName) {
+		question_Score.orderByChild("user").equalTo(userName).addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+					QuestionScore quest = postSnapshot.getValue(QuestionScore.class);
+					sum += Integer.parseInt(quest.getScore());
+				}
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError databaseError) {
+			
+			}
+		});
+		
 	}
 	
 }
