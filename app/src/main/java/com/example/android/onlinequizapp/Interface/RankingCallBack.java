b2e02c7cdc8ca7bfd8b4032dce7b7a06196eefbd
Ahmed Elhendawy <ahm_elhen@yahoo.com>
2018-03-22 14:48:03 +0200

    Ranking interface callback to process score value

---
+++ app/src/main/java/com/example/android/onlinequizapp/Interface/RankingCallBack.java
@@ -0,0 +1,10 @@
+package com.example.android.onlinequizapp.Interface;
+
+/**
+ * Created by Ahmed El Hendawy on 2018/03/22.
+ */
+
+public interface RankingCallBack<T> {
+	void callBack(T ranking);
+	
+}
